f087420dc227a5d17b73dd889382d77af4c78739
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-05 13:12:11 -0300

    Bump version: 0.1.1 → 1.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='erpbrasil.edoc.pdf',
-    version='0.1.1',
+    version='1.0.0',
     license='MIT',
     description='Impressão de documentos fiscais a partir do XML: NF-E, NFC-E, CT-E, MDF-E, GNRE e etc.',
     long_description='%s\n%s' % (
